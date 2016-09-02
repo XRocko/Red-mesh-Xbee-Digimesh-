9087b299dfd867e182a2428d9de5a5d1a8bbdbae
XRocko <hugo.mladineoc@gmail.com>
2016-09-02 17:21:21 -0300

    Rename Prueba2.ino to Recepción.ino

---
+++ Recepción.ino
@@ -0,0 +1,16 @@
+#include <SoftwareSerial.h>
+SoftwareSerial XBee(2, 3); // Arduino RX, TX (XBee Dout, Din)
+
+void setup() 
+{
+  XBee.begin(9600);
+  Serial.begin(9600);
+  }
+
+void loop()
+{
+    if(XBee.available()){      
+      char c = XBee.read();
+    Serial.print(c);
+  }
+  }
